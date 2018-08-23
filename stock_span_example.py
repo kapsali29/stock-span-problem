4fd61af2a8b66ab7d57957238f9c1fbfcb5c1170
Panagiotis Kapsalis <kapsalis@synelixis.com>
2018-08-23 18:50:16 +0300

    some fixes

--- stock_span_example.py
+++ stock_span_example.py
@@ -6,12 +6,13 @@ for i in range(len(stocks)):
     if i == 0:
         results = [1]
     else:
-        if stocks[i] >= stocks[i -1] && stocks[i] < stocks[max_value_pos_before_checkpoint]:
+        if stocks[i] >= stocks[i -1] and stocks[i] < stocks[max_value_pos_before_checkpoint]:
             results.append(i -max_value_pos_before_checkpoint)
-        elif stocks[i] <= stocks[i - 1]:
+        elif stocks[i] < stocks[i - 1]:
             results.append(1)
-            max_value_pos_before_checkpoint = current_pos - 1
-        elif stocks[i] >= stocks[i -1] && stocks[i] >= stocks[max_value_pos_before_checkpoint]:
+            max_value_pos_before_checkpoint = i - 1
+            print(max_value_pos_before_checkpoint)
+        elif stocks[i] >= stocks[i -1] and stocks[i] >= stocks[max_value_pos_before_checkpoint]:
             results.append(i -max_value_pos_before_checkpoint+results[max_value_pos_before_checkpoint])
 print(results)
 
